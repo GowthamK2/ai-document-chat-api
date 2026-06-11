16207d50d12f00f3ce511e6bb56dc44ed811aca2
Gowtham <gowthamkrissh24@gmail.com>
2026-06-11 20:33:15 +0530

    feat: add conversational memory to RAG pipeline

--- app/llm_service.py
+++ app/llm_service.py
@@ -1,13 +1,32 @@
 from ollama import chat
 
-def generate_answer(question: str, context: str):
-    prompt = f"""Answer the question using only the provided context"
 
-    context: {context}
-    Question: {question} """
+def generate_answer(
+    question: str,
+    context: str,
+    memory=""
+):
+
+    prompt = f"""
+You are a helpful assistant.
+
+Use the conversation history and document context.
+
+If the answer is not found in the context,
+say "I don't know based on the provided document."
+
+Conversation History:
+{memory}
+
+Context:
+{context}
+
+Question:
+{question}
+"""
 
     response = chat(
-        model = "phi3",
+        model="phi3",
         messages=[
             {
                 "role": "user",
@@ -15,4 +34,5 @@ def generate_answer(question: str, context: str):
             }
         ]
     )
+
     return response.message.content
--- app/rag_service.py
+++ app/rag_service.py
@@ -1,6 +1,6 @@
 from app.embeddings import get_embedding
 
-from app.chroma_services import(
+from app.chroma_services import (
     search_chunks
 )
 
@@ -8,9 +8,28 @@ from app.llm_service import (
     generate_answer
 )
 
+from app.memory_service import (
+    add_to_memory,
+    get_memory
+)
+
+
 def ask_question(question: str):
+
+    memory = get_memory()
+
+    history = ""
+
+    for item in memory:
+        history += (
+            item["question"] + " "
+        )
+    enhanced_query = (
+        history + question
+    )
+
     query_embedding = get_embedding(
-        question
+        enhanced_query
     )
 
     results = search_chunks(
@@ -22,9 +41,25 @@ def ask_question(question: str):
         results["documents"][0]
     )
 
+    memory = get_memory()
+
+    memory_text = ""
+
+    for item in memory:
+        memory_text += (
+            f"Question: {item['question']}\n"
+            f"Answer: {item['answer']}\n\n"
+        )
+
     answer = generate_answer(
         question,
-        context
+        context,
+        memory_text
+    )
+
+    add_to_memory(
+        question,
+        answer
     )
 
     return {
